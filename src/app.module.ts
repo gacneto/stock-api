952c2d2928d82ba1a3406a9575f162656fc9152b
Lucas Barcelar <lbarcelarc@gmail.com>
2025-09-16 11:28:19 -0300

    fix: final setup for production deploy with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,38 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductModule } from './product/product.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './product/product.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost', // Usamos 'localhost' porque a porta 3306 do container está exposta na sua máquina
-      port: 3306,
-      username: 'root',      // Credenciais do docker-compose.yml
-      password: 'root',      // Credenciais do docker-compose.yml
-      database: 'estoque_db',  // Nome do banco do docker-compose.yml
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // Esta linha verifica se estamos em ambiente de produção
+        const isProduction = configService.get('NODE_ENV') === 'production';
 
-      // Encontra automaticamente todos os arquivos .entity.ts no projeto
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-
-      // Sincroniza as entidades com o banco. Ótimo para dev, ruim para produção.
-      // Ele criará a tabela 'products' para nós automaticamente!
-      synchronize: true,
+        return {
+          type: 'postgres',
+          url: configService.get<string>('DATABASE_URL'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+         
+          ...(isProduction && {
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          }),
+        };
+      },
     }),
-    ProductModule],
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
